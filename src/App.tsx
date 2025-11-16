6eeff2daa9a1afee6fd90b506ae565e80e09b5fd
soulbreaker156 <soulbreaker0220@gmail.com>
2025-11-16 14:16:45 -0600

    feat: se agrego estilos predefinidos para toda la aplicacion

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
+import './App.css';
 export default function App() {
   return (
     <>
-      <h1 >Hello, World!</h1>
+      <h1 className="font-expose text-4xl text-center mt-15">Proyecto F1</h1>
     </>
   )
 }
